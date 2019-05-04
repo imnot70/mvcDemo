5b72c6992d7541a0c93f2eba2431e3008bfa8b58
imnot70 <754192687@qq.com>
2019-05-04 17:28:34 +0800

    test
    test

--- src/main/java/demo/thread/PoolDemo001.java
+++ src/main/java/demo/thread/PoolDemo001.java
@@ -2,17 +2,32 @@ package demo.thread;
 
 import org.junit.Test;
 
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 public class PoolDemo001 {
 
+    private int corePoolSize = 2;
+    private int maximumPoolSize = 10;
+    private int keepAliveTime = 1000*30;
 
     @Test
     public void test001(){
         ExecutorService exe = Executors.newCachedThreadPool();
+        for(int i = 0;i<10;i++){
+            exe.submit(new PoolThread());
+        }
+        exe.shutdown();
+    }
+
+    @Test
+    public void test002(){
+
+        ExecutorService exs = new ThreadPoolExecutor(corePoolSize,maximumPoolSize,keepAliveTime,TimeUnit.MILLISECONDS, new LinkedBlockingDeque<Runnable>());
+        for(int i = 0;i<10;i++){
+            exs.submit(new PoolThread("t-"+i));
+        }
+
+        exs.shutdown();
     }
 
 }
--- src/main/java/mvc/controller/TestController.java
+++ src/main/java/mvc/controller/TestController.java
@@ -1,9 +1,15 @@
 package mvc.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("test")
 public class TestController {
 
+    @RequestMapping("index")
+    public String toIndex(){
 
+        return "index";
+    }
 }
