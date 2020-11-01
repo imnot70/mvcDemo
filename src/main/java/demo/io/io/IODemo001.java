72b047adebc37011f1095d3e460a5a89436e53a9
ZhangPeng <754192687@qq.com>
2020-11-01 11:00:01 +0800

    test

--- src/main/java/demo/io/io/IODemo001.java
+++ src/main/java/demo/io/io/IODemo001.java
@@ -6,7 +6,7 @@ import java.io.*;
 
 public class IODemo001 {
 
-    @Test
+	@Test
     public void test001() throws Exception {
         String hello="hello,world!";
         File file = new File("hello.txt");
@@ -30,12 +30,24 @@ public class IODemo001 {
         BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(new File("简历6.docx")));
         byte[] bytes = new byte[8192];
         int flag = 0;
-        while((flag= bis.read(bytes)) != -1){
+        while((flag =bis.read(bytes)) != -1){
             bos.write(bytes,0,bytes.length);
         }
 
         bis.close();
         bos.close();
+    }
+
+    @Test
+    public void test004() throws Exception {
+    	InputStream is = new FileInputStream(new File("test.txt"));
+    	byte[] bytes = new byte[1024];
+    	OutputStream os = new FileOutputStream(new File("test_buffer.txt"));
+    	while(is.read(bytes) != -1) {
+    		os.write(bytes);
+    	}
+    	is.close();
+    	os.close();
     }
 
 }
